8596aa7853a55f3a1b3db743e2eb07d35338f83c
pheonix2121 <bobbypanwar73@gmail.com>
2023-08-10 21:26:52 +0530

    task 12 eccom

--- src/components/routePages/About.js
+++ src/components/routePages/About.js
@@ -1,8 +1,14 @@
 import React from "react";
-
+import Contact from "./ContactPage";
+import { NavLink } from "react-router-dom";
 const About = () => {
   return (
     <div>
+       <div>
+        <NavLink to="/contact" activeClassName="active">Contact Us</NavLink>
+      </div>
+      <Contact />
+
       <h1>ABOUT</h1>
       <p>
         Lorem ipsum carrots enhanced rebates. Excellent sayings of a man of
---
+++ src/components/routePages/ContactPage.js
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+
+const Contact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const data = {
+      name,
+      email,
+      phone,
+    };
+
+    try {
+      const response = await fetch(
+        "https://my-ecomm-demo-app-default-rtdb.firebaseio.com/eCommUser",
+        {
+          method: "POST",
+          body: JSON.stringify(data),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      if (!response.ok) {
+        throw new Error("Something went wrong....Retrying");
+      }
+
+      const responseData = await response.json();
+
+      console.log("Data stored successfully:", responseData);
+    } catch (error) {
+      console.error("Error storing data:", error);
+    }
+
+    setName("");
+    setEmail("");
+    setPhone("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Name:
+        <input
+          type="text"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        />
+      </label>
+      <br />
+      <label>
+        Email:
+        <input
+          type="email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+      </label>
+      <br />
+      <label>
+        Phone:
+        <input
+          type="tel"
+          value={phone}
+          onChange={(event) => setPhone(event.target.value)}
+        />
+      </label>
+      <br />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default Contact;
